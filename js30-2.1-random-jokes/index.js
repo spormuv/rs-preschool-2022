76f8cb8f8b3a2d59aafb781a5f07580e203ebd7d
coronarus <coronarus@mail.ru>
2022-02-12 11:49:21 +0300

    feat: add translation

--- js30-2.1-random-jokes/index.js
+++ js30-2.1-random-jokes/index.js
@@ -1,25 +1,52 @@
-//constants
-
+// constants
 const joke = document.querySelector('.joke');
 const btn = document.querySelector('.btn');
-const url =
-  'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
+const url = 'https://type.fit/api/quotes';
+const url2 = 'quotes.json';
+const langChange = document.querySelector('.lang-change');
+const wrapper = document.querySelector('.wrapper');
+let source = url;
 
-//get joke
-let getJoke = () => {
-  joke.classList.remove('fade');
+// function get and show data
+async function getData(url) {
+  joke.classList.remove('unfade');
+  const res = await fetch(url);
+  const data = await res.json();
+  let randJoke = data[Math.floor(Math.random() * data.length)];
+
   let col = function generateColor() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
   };
   document.body.style.backgroundColor = col();
-  fetch(url)
-    .then((data) => data.json())
-    .then((item) => {
-      joke.textContent = item.joke;
-      joke.classList.add('fade');
-    });
-};
-getJoke();
 
-//add listener
-btn.addEventListener('click', getJoke);
+  function showData() {
+    joke.textContent = randJoke.text;
+    joke.classList.add('unfade');
+  }
+  showData();
+}
+getData(source);
+
+// change language
+
+btn.addEventListener('click', () => getData(source));
+
+function rusLang() {
+  btn.textContent = 'Получить Цитату';
+  langChange.textContent = 'ru';
+  source = url2;
+  getData(source);
+}
+
+function engLang() {
+  btn.textContent = 'Get Quote';
+  langChange.textContent = 'en';
+  source = url;
+  getData(source);
+}
+
+langChange.addEventListener('click', () => {
+  wrapper.classList.toggle('rus');
+  const isLang = wrapper.classList.contains('rus');
+  isLang ? rusLang() : engLang();
+});
